1c8c4d1cc19e2b10bda820d9eed45c93eb37bb9a
Juan Gómez <gomezj01@stepstone.com>
2023-09-01 15:00:29 -0600

    Validación del rol contra la base de datos

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
+const Roles = require('../models/Roles')
 const { validateFields } = require('../middleware/validateFields')
 const { getUser, postUser, putUser, deleteUser, patchUser } = require('../controllers/user.controller')
 
@@ -11,7 +12,12 @@ router.post('/', [
 	check('name', 'Name is required').not().isEmpty(),
 	check('email', 'Email is not valid').isEmail(),
 	check('password', 'Password is required and must be more than 6 characters').isLength({ min: 6 }),
-	check('role', 'This role is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+	check('role', custom( async (role = '') => { 
+		const roleExists = await Roles.findOne({ role })
+		if(!roleExists) {
+			throw new Error(`${ role } role is not valid`)
+		}
+	} )),
 	validateFields
 ], postUser)
 
