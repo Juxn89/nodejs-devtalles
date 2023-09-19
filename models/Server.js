97526d2a1bc340e3560ca88b9b4e3034aeaa4fe5
Juan Gómez <gomezj01@stepstone.com>
2023-09-19 10:08:33 -0600

    Enviar información de frontend al servidor

--- models/Server.js
+++ models/Server.js
@@ -44,6 +44,14 @@ class Server {
 	sockets() {
 		this.io.on('connection', socket => {
 			console.log('Client connected!', socket.id)
+
+			socket.on('disconnect', () => {
+				console.log('Cliend disconnected', socket.id)
+			})
+
+			socket.on('send-message', (paylaod) => {
+				console.log('Message recived from client:', paylaod)
+			})
 		})
 	}
 }
--- public/scripts/socket-client.js
+++ public/scripts/socket-client.js
@@ -3,6 +3,9 @@ const socket = io();
 const online = document.querySelector('#lblOnline')
 const offline = document.querySelector('#lblOffline')
 
+const txtMessage = document.querySelector('#txtMessage')
+const btnSend = document.querySelector('#btnSend')
+
 socket.on('connect', () => {
 	console.log('Client connected!')
 	offline.style.display = 'none'
@@ -13,4 +16,16 @@ socket.on('disconnect', () => {
 	console.log('Client disconnected!')
 	offline.style.display = ''
 	online.style.display = 'none'
+})
+
+btnSend.addEventListener('click', () => {
+	const message = txtMessage.value
+
+	const payload = {
+		message,
+		id: '123ABC',
+		date: new Date().getTime()
+	}
+
+	socket.emit('send-message', payload)
 })
