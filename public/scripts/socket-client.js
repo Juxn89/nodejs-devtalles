cda027c9fda09c82d6e7a7bffb1533227170ecf5
Juan Gómez <gomezj01@stepstone.com>
2023-09-19 10:18:33 -0600

    Enviar mensajes desde el server a clientes

--- public/scripts/socket-client.js
+++ public/scripts/socket-client.js
@@ -7,15 +7,19 @@ const txtMessage = document.querySelector('#txtMessage')
 const btnSend = document.querySelector('#btnSend')
 
 socket.on('connect', () => {
-	console.log('Client connected!')
+	// console.log('Client connected!')
 	offline.style.display = 'none'
 	online.style.display = ''
 })
 
 socket.on('disconnect', () => {
-	console.log('Client disconnected!')
+	// console.log('Client disconnected!')
 	offline.style.display = ''
 	online.style.display = 'none'
+})
+
+socket.on('notify-message', payload => {
+	console.log('Notify message:', payload)
 })
 
 btnSend.addEventListener('click', () => {
