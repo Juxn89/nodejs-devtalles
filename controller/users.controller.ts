f17b932813ddffc93a0af170e4a7eadb44c1e724
Juan Gómez <gomezj01@stepstone.com>
2023-09-28 09:16:28 -0600

    CURD de usuarios

--- controller/users.controller.ts
+++ controller/users.controller.ts
@@ -1,27 +1,81 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
-export const getUsers = (req: Request, res: Response) => {
-	res.status(200).json({ msg: 'List of users :)' })
+export const getUsers = async (req: Request, res: Response) => {
+	const users = await User.findAll()
+
+	res.status(200).json({ users })
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
 	const { id } = req.params
-	res.status(200).json({ msg: 'Users', id })
+
+	const user = await User.findByPk(id)
+
+	if(!user)
+		return res.status(404).json({ msg: `User with ID: ${ id } doesn't exist.` })
+
+	res.status(200).json({ user })
 }
 
-export const saveUser = (req: Request, res: Response) => {
+export const saveUser = async (req: Request, res: Response) => {
 	const { body } = req
-	res.status(200).json({ msg: 'Post User', body })
+
+	try {
+		const verifyEmail = await User.findOne({ 
+			where: { email: body.email }
+		})
+
+		if(verifyEmail)
+			return res.status(400).json({ msg: `Email '${body.email}' already exists in db.` })
+
+		const user = User.build(body)
+		user.save()
+
+		res.status(200).json({ user })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ msg: 'Reach out with administrator' })
+	}
 }
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
+	const { id } = req.params
+	const { body } = req	
+
+	try {
+		const user = await User.findByPk(id);
+
+		if(!user)
+			return res.status(404).json({ msg: `User with ID : ${id} not found` })
+
+		await user.update(body)
+
+		res.status(200).json({ user })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ msg: 'Reach out with administrator' })
+	}
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
 	const { id } = req.params
 	const { body } = req
-	res.status(200).json({ msg: 'Update User', body })
-}
+	
+	try {
+		const user = await User.findByPk(id);
 
-export const deleteUser = (req: Request, res: Response) => {
-	const { id } = req.params
-	const { body } = req
-	res.status(200).json({ msg: 'Delete User', body })
+		if(!user)
+			return res.status(404).json({ msg: `User with ID : ${id} not found` })
+
+		await user.update({...body, isActive: false})
+
+		res.status(200).json({ user })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ msg: 'Reach out with administrator' })
+	}
 }
